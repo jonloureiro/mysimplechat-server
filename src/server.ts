ec6f086038c85fe78ba3b90e1bb79b3d8940bf97
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2019-07-04 02:59:18 -0400

    iniciando um server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log('hello world');
+const consola = require('consola');
+
+const server = require('http').createServer();
+
+const { port } = require('./config');
+
+server.listen(port, (): void => {
+  consola.success('Server running...');
+  consola.info(`http://localhost:${server.address().port}\n`);
+});
